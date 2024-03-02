b7fc038f851621f3ae5a9b329f2bfd2b92b384f0
Ekaterina Ustinova <galateya2406@yandex.ru>
2024-03-02 23:17:20 +0400

    Обновления 0203

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -4,6 +4,7 @@ import AppHeader from '../app-header/app-header';
 import SearchPanel from '../search-panel/search-panel';
 import TodoList from '../todo-list/todo-list';
 import ItemStatusFilter from '../item-status-filter/item-status-filter';
+import ItemAddForm from '../item-add-form/item-add-form';
 
 import './app.css';
 
@@ -46,6 +47,8 @@ export default class App extends Component {
         <TodoList 
         todos={this.state.todoData} 
         onDeleted = { this.deleteItem }/>
+
+        <ItemAddForm />
       </div>
     )
   }
